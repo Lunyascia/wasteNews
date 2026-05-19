2abb5d91a5ed26c1c410bdf59434137df9104f7c
Lunyascia <3311352500@qq.com>
2026-05-19 13:58:34 +0800

    2
    模块化路由

--- routers/news.py
+++ routers/news.py
@@ -3,5 +3,10 @@ from fastapi import APIRouter
 router = APIRouter( prefix="/api/news", tags=["news"])
 
 @router.get("/categories")
-def get_news_categories():
-    return {"msg": "news categories"}
+async def get_categories(skip: int = 0, limit: int = 100):
+    return {
+        "code":200,
+        "message":"获取成功",
+        "data":"新闻分类列表"
+
+    }
