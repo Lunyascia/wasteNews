3aef6dd726e4553ced9e5a2584ec0026e7a64907
Lunyascia <3311352500@qq.com>
2026-05-23 11:51:07 +0800

    5
    添加中间件CORS,完成跨域HTTP请求

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from routers import news
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # 挂载路由
 app.include_router(news.router)
