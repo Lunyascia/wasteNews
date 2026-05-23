32473d88af2e1db350879a0724086d6b879fc5a8
Lunyascia <3311352500@qq.com>
2026-05-23 12:43:34 +0800

    8
    分页逻辑：计算偏移量，并获取指定页的数据，返回结果，添加hasMore字段

--- crud/news.py
+++ crud/news.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from models.news import Category
+from sqlalchemy import func
+from models.news import Category,News
 
 
 
@@ -8,3 +9,15 @@ async def get_categories(db:AsyncSession,skip: int = 0, limit: int = 100):
     stat = select(Category).offset(skip).limit(limit)
     result = await db.execute(stat)
     return result.scalars().all()
+
+async def get_news_list(db:AsyncSession,category_id: int,skip: int = 0,limit: int = 10):
+    stat = select(News).where(News.category_id == category_id).offset(skip).limit(limit)
+    result = await db.execute(stat)
+    return result.scalars().all()
+
+async def get_news_count(db:AsyncSession,category_id: int):
+    stat = select(func.count(News.id)).where(News.category_id == category_id)
+    result = await db.execute(stat)
+    return result.scalar()
+
+
--- routers/news.py
+++ routers/news.py
@@ -1,5 +1,3 @@
-from email.policy import default
-
 from fastapi import APIRouter,Depends,Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -23,13 +21,23 @@ async def get_categories(skip: int = 0, limit: int = 100,db: AsyncSession = Depe
 
 @router.get("/list")
 async def get_news_list(
-        category_id: int = Query(default:=0,alias="categoryId"),
-        page: int = 0,
-        page_size: int = Query(default=10,alias="pageSize",le=100),
-        db: AsyncSession = Depends(get_db))\
-        :
+        category_id: int = Query(default=1, alias="categoryId"),
+        page: int = 1,
+        page_size: int = Query(default=10, alias="pageSize", le=100),
+        db: AsyncSession = Depends(get_db)
+):
+
+# 分页逻辑：计算偏移量，并获取指定页的数据，返回结果，添加hasMore字段
+    offset = (page - 1) * page_size
+    news_list = await news.get_news_list(db, category_id, offset, page_size)
+    total = await news.get_news_count(db, category_id)
+    has_more = total > offset + page_size
     return {
-        "list":"新闻列表",
-        "total": "总量",
-        "hasmore":"是否有更多",
+        "code": 200,
+        "message": "获取成功",
+        "data": {
+            "list": news_list,
+            "total": total,
+            "hasMore": has_more,
+        }
     }
