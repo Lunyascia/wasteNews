a71ea6607835ecaeab04f505ec88168ad86af7fe
Lunyascia <3311352500@qq.com>
2026-05-23 12:16:27 +0800

    6
    获取新闻列表

--- routers/news.py
+++ routers/news.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter,Depends
+from email.policy import default
+
+from fastapi import APIRouter,Depends,Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from config.db_conf import get_db
 from crud import news
+
+
 
 router = APIRouter( prefix="/api/news", tags=["news"])
 
@@ -14,4 +18,18 @@ async def get_categories(skip: int = 0, limit: int = 100,db: AsyncSession = Depe
         "message":"获取成功",
         "data":categories
 
+    }
+
+
+@router.get("/list")
+async def get_news_list(
+        category_id: int = Query(default:=0,alias="categoryId"),
+        page: int = 0,
+        page_size: int = Query(default=10,alias="pageSize",le=100),
+        db: AsyncSession = Depends(get_db))\
+        :
+    return {
+        "list":"新闻列表",
+        "total": "总量",
+        "hasmore":"是否有更多",
     }
