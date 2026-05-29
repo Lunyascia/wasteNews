10716c6c26642c98b8db901281b789ca6a48aa17
Lunyascia <3311352500@qq.com>
2026-05-29 11:35:08 +0800

    17
    生成token，设置过期时间是七天，查询数据库当前用户是否有token，有就更新没有就添加，出现问题创建用户后用户登录界面自己白屏，无法进行后续操作

--- crud/users.py
+++ crud/users.py
@@ -1,6 +1,9 @@
+import uuid
+from datetime import datetime, timedelta
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from models.users import User
+
+from models.users import User, UserToken
 from schemas.users import UserRequest
 from utils import security
 
@@ -29,3 +32,23 @@ async def create_user(db: AsyncSession, user_data: UserRequest):
     await db.refresh(user)
     await db.commit()
     return user
+
+async def create_token(db: AsyncSession,user_id: int):
+    token = str(uuid.uuid4())
+    expires_at = datetime.now() + timedelta(days=7)
+    query = select(UserToken).where(UserToken.user_id == user_id)
+    result = await db.execute(query)
+    user_token = result.scalar_one_or_none()
+    if user_token:
+        user_token.token = token
+        user_token.expires_at = expires_at
+    else:
+        user_token = UserToken(
+            user_id=user_id,
+            token=token,
+            expires_at=expires_at
+        )
+        db.add(user_token)
+        await db.commit()
+
+    return token
--- routers/users.py
+++ routers/users.py
@@ -1,9 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
-
 from config.db_conf import get_db
-from crud.users import get_user_by_username, create_user
+from crud.users import get_user_by_username, create_user,create_token
 from schemas.users import UserRequest
 
 router = APIRouter ( prefix = "/api/user" , tags = [ "users" ])
@@ -16,12 +15,12 @@ async def register (user_data:UserRequest,db: AsyncSession = Depends (get_db),):
         
         # 修正：调用 create_user 函数并传入 Pydantic 对象
         user = await create_user(db, user_data)
-
+        token = await create_token(db, user.id)
         return {
             "code": 200,
             "message": "注册成功",
             "data": {
-            "token":"用户访问名牌",
+            "token":token,
             "userinfo": {
             "id": user.id,
             "username": user.username,
